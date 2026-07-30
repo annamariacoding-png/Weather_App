b266eefddd3c67772067fa71a4c4040b1b484391
Anna Maria <annamaria.academic@gmail.com>
2026-07-30 14:31:33 +0530

    UI update (Storing data  in LocalStorage)

--- Interface.js
+++ Interface.js
@@ -1,16 +1,25 @@
 let selection = true;
+
+if (JSON.parse(localStorage.getItem("cityname")) !== null) {
+    cityChange = JSON.parse(localStorage.getItem("cityname"));
+    document.querySelector('.City-name').innerHTML = `📍 ${cityChange.cityname}`;
+}
+
 function showPopup() {
     document.querySelector('.Pop-up').classList.add('show');
 }
+
 function hidePopup() {
+    console.log("Hide pop up called");
     document.querySelector('.Pop-up').classList.remove('show');
     document.querySelector('.Search-Bar').value = '';
     selection = true;
 }
-const weather = {
-    city: "Kochi"
-};
+
 function selectcity(city) {
+    let details = {
+    "cityname" : city
+    }
+    localStorage.setItem("cityname", JSON.stringify(details))
     document.querySelector('.City-name').innerHTML = `📍 ${city}`;
-    localStorage.setItem("details", JSON.stringify(weather));
 }
